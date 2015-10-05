456d177fb10eb1e7d84904b5eea2a2d37b8d422c
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2015-10-05 14:20:35 -0300

    setup.py adicionado

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+      name='pyterm',
+      version='0.1',
+      description='PyTerm is a terminal client for linux, that show a hightlight on the availables commands of your computer',
+      url='https://github.com/jeffersonmourak/pyTerm',
+      author='Jefferson Moura',
+      author_email='jefinho.moura14@gmail.com',
+      license='MIT',
+      packages=[],
+      install_requires=[],
+      zip_safe=False
+)
